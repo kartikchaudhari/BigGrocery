31fa0b8c7646da06dbe212052242e833d094c7e7
Kartik <kartikchaudhari456@gmail.com>
2020-04-16 11:42:28 +0530

    Update config_paytm.php

--- application/third_party/PaytmKit/lib/config_paytm.php
+++ application/third_party/PaytmKit/lib/config_paytm.php
@@ -8,8 +8,8 @@
 */
 
 define('PAYTM_ENVIRONMENT', 'TEST'); // PROD
-define('PAYTM_MERCHANT_KEY', '7ClcVQ7vEcbTboNE'); //Change this constant's value with Merchant key received from Paytm.
-define('PAYTM_MERCHANT_MID', 'DIiaKw58338439656916'); //Change this constant's value with MID (Merchant ID) received from Paytm.
+define('PAYTM_MERCHANT_KEY', '##'); //Change this constant's value with Merchant key received from Paytm.
+define('PAYTM_MERCHANT_MID', '##'); //Change this constant's value with MID (Merchant ID) received from Paytm.
 define('PAYTM_MERCHANT_WEBSITE', 'WEBSTAGING'); //Change this constant's value with Website name received from Paytm.
 
 $PAYTM_STATUS_QUERY_NEW_URL='https://securegw-stage.paytm.in/merchant-status/getTxnStatus';
